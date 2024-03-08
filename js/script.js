8e93024aca9eb73f96a05d3ffae5b8a02b0e55a7
Elias <eliasasg@gmail.com>
2024-03-08 16:30:36 -0600

    Aba

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log("Merge Exercise");
+function myFunction(){
+    let data = "";  let name = document.getElementById("userName").value
+    let email = document.getElementById("userEmail").value
+    let comment = document.getElementById("userComment").value
+
+data = "User name : "+name+"<br/>User email : "+email+ "<br/>User comment : "+comment
+
+document.getElementById("data").innerHTML = data  // display data to paragraph
+}
